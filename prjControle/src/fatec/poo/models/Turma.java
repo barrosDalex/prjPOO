2ac663cedd73dc736de5334e9c72fd132904d7ad
Hugo Morales <hugo.camargo87@gmail.com>
2018-10-24 18:50:08 -0300

    Classe Turma.

---
+++ prjControle/src/fatec/poo/models/Turma.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fatec.poo.models;
+
+/**
+ *
+ * @author Hugo
+ */
+public class Turma {
+    private String SiglaTurma;
+    private String Descricao;
+    private String DataInicio;
+    private String DataTermino;
+    private String Periodo;
+    private int QtdVagas;
+    private String Observacoes;
+
+    public Turma(String SiglaTurma, String Descricao) {
+        this.SiglaTurma = SiglaTurma;
+        this.Descricao = Descricao;
+    }
+
+    public void setDescricao(String Descricao) {
+        this.Descricao = Descricao;
+    }
+
+    public void setDataInicio(String DataInicio) {
+        this.DataInicio = DataInicio;
+    }
+
+    public void setDataTermino(String DataTermino) {
+        this.DataTermino = DataTermino;
+    }
+
+    public void setPeriodo(String Periodo) {
+        this.Periodo = Periodo;
+    }
+
+    public void setQtdVagas(int QtdVagas) {
+        this.QtdVagas = QtdVagas;
+    }
+
+    public void setObservacoes(String Observacoes) {
+        this.Observacoes = Observacoes;
+    }
+    
+    public void emitirListaFrequencia(){
+        //Não implementar nesse trabalho
+    }
+}
