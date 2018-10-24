4900f16d5e97928be1725ae1a1d449db0033e6dd
Hugo Morales <hugo.camargo87@gmail.com>
2018-10-24 19:07:20 -0300

    Classe Curso.

---
+++ prjControle/src/fatec/poo/models/Curso.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fatec.poo.models;
+
+/**
+ *
+ * @author Hugo
+ */
+public class Curso {
+    private String Sigla;
+    private String Nome;
+    private int CargaHoraria;
+    private double Valor;
+    private String DataVigencia;
+    private double ValorHoraInstrutor;
+    private String Programa;
+
+    public Curso(String Sigla, String Nome) {
+        this.Sigla = Sigla;
+        this.Nome = Nome;
+    }
+
+    public void setCargaHoraria(int CargaHoraria) {
+        this.CargaHoraria = CargaHoraria;
+    }
+
+    public void setValor(double Valor) {
+        this.Valor = Valor;
+    }
+
+    public void setDataVigencia(String DataVigencia) {
+        this.DataVigencia = DataVigencia;
+    }
+
+    public void setValorHoraInstrutor(double ValorHoraInstrutor) {
+        this.ValorHoraInstrutor = ValorHoraInstrutor;
+    }
+
+    public void setPrograma(String Programa) {
+        this.Programa = Programa;
+    }
+}
