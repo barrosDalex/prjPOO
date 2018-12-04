8784ac81657bf52bf8c1ad819460f3492a836e49
AlexBarroSS <alex.silva123@fatec.sp.gov.br>
2018-12-04 09:53:17 -0200

    EDIT bd

--- TABLE.sql
+++ TABLE.sql
@@ -2,20 +2,9 @@
 
 -- Tabelas
 
---Se precisar limpar
-
-drop table tb_turma;
-drop table tb_curso;
-drop table tb_matricula;
-drop table tb_aprazo;
-drop table tb_avista;
-drop table tb_instrutor;
-drop table tb_aluno;
-drop table tb_pessoa;
-
 create table tb_Pessoa(
 	nome varchar2(30) not null,
-	dataNasc varchar2(10),
+	dataNasc varchar2(15),
 	endereco varchar2(50),
 	numero number(5),
 	bairro varchar2(20),
@@ -24,12 +13,11 @@ create table tb_Pessoa(
 	cep varchar2(8),
 	telefone varchar2(10),
 	celular varchar2(11),
-	sexo varchar2(1) not null, -- CHK
+	sexo varchar2(9) not null, -- CHK
 	estadoCivil varchar2(15),
 	rg varchar2(9) not null, -- UQ
 	cpf varchar2(11) not null, -- PK
-	email varchar2(30),
-	tipoPessoa varchar2(10) not null);
+	email varchar2(30));
 
 create table tb_Instrutor(
 	cpf varchar2(11) not null, -- PK FK
@@ -45,49 +33,47 @@ create table tb_Curso(
 	nome varchar2(20) not null,
 	cargaHoraria number(4),
 	valor number(7, 2) not null,
-	dtVigencia varchar2(11),
+	dtVigencia varchar2(15),
 	valorHoraInstrutor number(7, 2) not null,
 	programa varchar2(50));
 	
 create table tb_Turma(
 	siglaTurma varchar2(5) not null, -- PK
 	descricao varchar2(30) not null,
-	dtInicio varchar2(11),
-	dtTermino varchar2(11),
+	dtInicio varchar2(15),
+	dtTermino varchar2(15),
 	periodo varchar2(8) not null,
 	qtdeVagas number(3) not null,
 	observacoes varchar2(50),
-	cpfInstrutor varchar2(11) not null, -- FK
+	cpfInstrutor varchar2(11), -- FK
 	siglaCurso varchar2(5) not null); -- FK
 	
 create table tb_AVista(
-	codAvista number(10) not null, -- PK 
+	cpfMatricula varchar2(11) not null, -- PK FK 
 	valor number(7, 2) not null,
 	agencia number(5) not null,
 	nCheque number(8) not null,
-	preDt varchar2(10) not null);
+	preDt varchar2(15) not null);
 
 create table tb_APrazo(
-	codAprazo number(10) not null, -- PK
+	cpfMatricula varchar2(11) not null, -- PK FK
 	valor number(7, 2) not null,
-	dtVencimento varchar2(10) not null,
+	dtVencimento varchar2(15) not null,
 	taxaJuros number(5, 2) not null,
 	qtdeMensalidade number(3) not null);
 
 create table tb_Matricula(
 	cpf varchar2(11) not null, -- PK FK
-	dtMatricula varchar2(10),
+	dtMatricula varchar2(15),
 	nota number(4, 2) not null,
-	siglaTurma varchar2(5) not null, -- FK
-	codAvista number(10), -- FK
-	codAprazo number(10)); -- FK
+	qtdeFaltas number(3),
+	siglaTurma varchar2(5) not null); -- FK
 
 	
 -- Constraints
 
 -- Pessoa
 alter table tb_Pessoa add constraint PK_Pessoa_cpf primary key (cpf);
-alter table tb_Pessoa add constraint CHK_Pessoa_sexo check (sexo in ('M', 'F'));
 alter table tb_Pessoa add constraint CHKU_Pessoa_rg unique (rg);
 
 -- Instrutor
@@ -108,13 +94,13 @@ alter table tb_Turma add constraint FK_Turma_siglaCurso foreign key (siglaCurso)
 
 -- AVista
 alter table tb_AVista add constraint PK_AVista primary key (codAvista);
+alter table tb_AVista add constraint FK_AVista_Matricula foreign key (cpfMatricula) references tb_Matricula(cpf);
 
 -- APrazo
 alter table tb_APrazo add constraint PK_APrazo primary key (codAprazo);
+alter table tb_APrazo add constraint FK_APrazo_Matricula foreign key (cpfMatricula) references tb_Matricula(cpf);
 
 -- Matricula
 alter table tb_Matricula add constraint FK_Matricula_cpf foreign key (cpf) references tb_Aluno;
 alter table tb_Matricula add constraint PK_Matricula_cpf primary key (cpf);
 alter table tb_Matricula add constraint FK_Matricula_siglaTurma foreign key (siglaTurma) references tb_Turma;
-alter table tb_Matricula add constraint FK_Matricula_codAvista foreign key (codAvista) references tb_AVista;
-alter table tb_Matricula add constraint FK_Matricula_codAprazo foreign key (codAprazo) references tb_APrazo;
